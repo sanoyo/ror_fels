1167bf44bb4228c3fa501cd776b33ce332926a23
Inah San Juan <inahsanjuan@gmail.com>
2018-03-14 11:07:40 +0800

    Implement basic login and signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user,  only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Successfully signed up!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
